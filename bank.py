daf579148bec79f3dd7295033ff8b79c24fa8c20
Anees-DevOps <anees.exams@gmail.com>
2025-03-27 16:29:03 +0530

    Update bank.py

--- bank.py
+++ bank.py
@@ -3,9 +3,6 @@
     #return balance - amount  # ❌ Bug: No check for negative balance!
 
 #After fixing the bug ---
-def withdraw(amount, balance):
-    return balance - amount  # ❌ Bug: No check for negative balance!
-
 # def withdraw(amount, balance):
 #     if amount > balance:
 #         raise ValueError("Insufficient funds!")  # ✅ Prevents negative balance
